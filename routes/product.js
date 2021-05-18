0da18e33e4828e18366ae14e53ae571aba4461ea
Souvikdeb2612 <souvikdeb26@gmail.com>
2021-05-18 11:03:24 +0530

    added show page

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,60 @@ const router=express.Router();
 const Product = require('../models/product');
 
 
-router.get('/products', async(req,res)=>{
-    const products = await Product.find(req.params.category); 
-    res.render('products/index', {products});
-    console.log(req.params);
+// Get the form for adding new product
+router.get('/products/new', (req, res) => {
+
+    res.render('products/new');
 })
 
+
+// Show particular product
+router.get('/products/:id', async(req, res) => {
+    try {
+        const product=await Product.findById(req.params.id)
+        res.render('products/show', { product});
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+})
+
+
+// Show products based on categories
+router.get('/products/:category/category', async(req,res)=>{
+    const products = await Product.find({category : req.params.category}); 
+    res.render('products/index', {products});
+    
+})
+
+
+
+
+// Create New Product
+router.post('/products',async(req, res) => {
+
+    try {
+        await Product.create(req.body.product);
+        res.redirect('/products');
+    }
+    catch (e) {
+        console.log(e.message);
+
+    } 
+});
+
+// Get Product Edit form
+router.post('/products/:id/edit',async(req, res) => {
+
+    try {
+        const product=await Product.findById(req.params.id);
+        res.render('products/edit', {product});
+    }
+    catch (e) {
+        console.log(e.message);
+
+    } 
+});
 
 
 
